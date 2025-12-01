df0e8dfa382d429ddcfb810644a9f32ba81e0b77
adeta92 <aydandeniztaskiran@gmail.com>
2025-12-01 16:10:10 +0300

    layout güncellemesi

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css"; // BU SATIR EKSİKTİ, ARTIK VAR ✅
+import "./globals.css"; // ARTIK BU SATIR AKTİF ✅
 
 const inter = Inter({ subsets: ["latin"] });
 
